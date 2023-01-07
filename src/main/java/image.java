0e52b515dbd893cb8dca9fa2f2f4fb1564d0bd42
ritu bashire <britu1992@gmail.com>
2023-01-07 13:55:11 +0530

    Image file adding

---
+++ src/main/java/image.java
@@ -0,0 +1,9 @@
+package com.example.docker;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+@RestController
+public class image {
+    @GetMapping ("/image")
+    public String getData() { return "Rahul's first docker image";}
+}
