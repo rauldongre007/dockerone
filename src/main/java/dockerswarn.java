a5d3cf184154219798228adb2ccb526fd9c67fa0
ritu bashire <britu1992@gmail.com>
2023-01-07 20:55:21 +0530

    docker swarn added

---
+++ src/main/java/dockerswarn.java
@@ -0,0 +1,10 @@
+
+package com.example.docker;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+public class dockerswarn {
+    @GetMapping ("/dockerswarn")
+    public String getData() { return " Docker swarn test page";}
+}
