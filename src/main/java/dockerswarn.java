f9cad8508da5e31ca37956240134390889c470e1
rauldongre007 <118907244+rauldongre007@users.noreply.github.com>
2023-01-11 17:44:26 +0530

    Update dockerswarn.java

--- src/main/java/dockerswarn.java
+++ src/main/java/dockerswarn.java
@@ -6,5 +6,5 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class dockerswarn {
     @GetMapping ("/dockerswarn")
-    public String getData() { return " Docker swarn test pagessssssssssssssssssssssssssssss";}
+    public String getData() { return " Docker swarn test pages";}
 }
